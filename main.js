f8e9488c59971c51b71a15dfab1d09080a0d28c2
Oskar Jaworski <ojaworski@rockitworks.pl>
2022-11-03 14:13:04 +0100

    Styles changes, responsive layout and load listener to localstorage done

--- main.js
+++ main.js
@@ -1,38 +1,30 @@
-// let todolist = [
-//     { description: 'Tidy your room', date: new Date('2022-11-15') },
-//     { description: 'Vacuum the living room', date: new Date('2022-11-02') },
-//     { description: 'Take out the trash', date: new Date('2022-10-07') }
-// ];
-//
-// function generateTableHead(table, data) {
-//     let thead = table.createTHead();
-//     let row = thead.insertRow();
-//     for (let key of data) {
-//         let th = document.createElement("th");
-//         let text = document.createTextNode(key);
-//         th.appendChild(text);
-//         row.appendChild(th);
-//     }
-// }
-//
-// function generateTable(table, data) {
-//     for (let element of data) {
-//         let row = table.insertRow();
-//         for (key in element) {
-//             let cell = row.insertCell();
-//             let text = document.createTextNode(element[key]);
-//             cell.appendChild(text);
-//         }
-//     }
-// }
-//
-// let table = document.querySelector("table");
-// let data = Object.keys(todolist[0]);
-//
-// generateTableHead(table, data);
-// generateTable(table, todolist);
-//
-//
-// let mainTable = (table, data) => {
-//
-// };
+window.addEventListener('load', () => {
+    todos = JSON.parse(localStorage.getItem('todos')) || [];
+    const nameInput = document.querySelector('#name');
+    const newTodoForm = document.querySelector('#addTodoForm')
+
+   newTodoForm.addEventListener('submit', e => {
+       e.preventDefault();
+
+       const todo = {
+           content: e.target.elements.addTodoContent.value,
+           date: e.target.elements.addTodoDate.value,
+           done: false,
+           createdAt: new Date().getTime()
+       }
+
+       todos.push(todo);
+
+       localStorage.setItem('todos', JSON.stringify(todos));
+
+       e.target.reset();
+
+       displayTodos();
+   })
+
+    displayTodos()
+})
+
+function displayTodos() {
+    
+}
